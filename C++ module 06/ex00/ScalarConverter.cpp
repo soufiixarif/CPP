be5e8a9130419e3b9ee54d4260cc0f0f79255084
soufiix <soufiix.arif>
2025-05-19 14:05:40 +0100

    to continue in linux

--- C++ module 06/ex00/ScalarConverter.cpp
+++ C++ module 06/ex00/ScalarConverter.cpp
@@ -26,6 +26,90 @@ bool isInt(std::string literal){
     return true;
 }
 
-void ft_cout(){}
+bool isFloat(std::string literal){
+    if (literal[literal.length() - 1] != 'f')        
+        return false;
+    literal.pop_back();
+    int i = 0;
+    int count = 0;
+    if (literal[i] == '+' || literal[i] == '-')
+        i++;
+    int tmp = i;
+    while (literal[i]){
+        if (literal[i] == '.')
+            count++;
+        if (count > 1)
+            return false;
+        i++;
+    }
+    i = tmp;
+    while (literal[i]){
+        if (!isdigit(literal[i]) && literal[i] != '.')
+            return false;
+        i++;
+    }
+    return true;
+}
 
-void ScalarConverter::convert(std::string literal){}
+bool isDouble(std::string literal){
+    int i = 0;
+    int count = 0;
+    if (literal[i] == '+' || literal[i] == '-')
+        i++;
+    int tmp = i;
+    while (literal[i]){
+        if (literal[i] == '.')
+            count++;
+        if (count > 1)
+            return false;
+        i++;
+    }
+    i = tmp;
+    while (literal[i]){
+        if (!isdigit(literal[i]) && literal[i] != '.')
+            return false;
+        i++;
+    }
+    return true;
+}
+
+void ft_cout(std::string info, bool impossible, char c, int i, float f, double d){
+    (void)c;
+    (void)i;
+    (void)f;
+    (void)d;
+    (void)info;
+    if (impossible){
+        std::cout << "char: impossible\n" << "int: impossible\n" << "float: nanf\n" << "double: nan" << std::endl;  
+    }
+
+}
+
+void ScalarConverter::convert(std::string literal){
+    std::string info;
+    char c = 0;
+    int i = 0;
+    float f = 0.0f;
+    double d = 0.0;
+    bool impossible = false;
+    if (literal == "-inff" || literal == "+inff" || literal == "nanf" || \
+        literal == "-inf" || literal == "+inf" || literal == "nan")
+            impossible = true;
+    if (isChar(literal)){
+        std::cout << "char" << std::endl;
+        info = "char";
+        c = literal[0];§
+        i = static_cast<int>(c);
+        f = static_cast<float>(c);
+        d = static_cast<double>(c);
+    }
+    else if (isInt(literal))
+        std::cout << "int" << std::endl;
+    else if (isFloat(literal))
+        std::cout << "float" << std::endl;
+    else if(isDouble(literal))
+        std::cout << "double" << std::endl;
+    else
+        std::cout << "tkhwira" << std::endl;
+    ft_cout(info, impossible, c,i,f,d);
+}
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-    float f = 5.67;
-    int i = 5 ; // Converts float to int
-    // float j = static_cast<float>(c);
-    char c = static_cast<char>(i);
-    std::cout << c << std::endl;
-}
