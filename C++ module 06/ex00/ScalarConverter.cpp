294143f4cfe2d683881bced56cf95cf575fb7c2b
soufiixarif <arif.soufiix@gmail.com>
2025-05-20 13:18:45 +0100

    to continue

--- C++ module 06/ex00/ScalarConverter.cpp
+++ C++ module 06/ex00/ScalarConverter.cpp
@@ -29,7 +29,7 @@ bool isInt(std::string literal){
 bool isFloat(std::string literal){
     if (literal[literal.length() - 1] != 'f')        
         return false;
-    literal.pop_back();
+    literal.resize(literal.size() - 1);
     int i = 0;
     int count = 0;
     if (literal[i] == '+' || literal[i] == '-')
@@ -73,43 +73,92 @@ bool isDouble(std::string literal){
     return true;
 }
 
-void ft_cout(std::string info, bool impossible, char c, int i, float f, double d){
-    (void)c;
-    (void)i;
-    (void)f;
-    (void)d;
-    (void)info;
-    if (impossible){
-        std::cout << "char: impossible\n" << "int: impossible\n" << "float: nanf\n" << "double: nan" << std::endl;  
-    }
+bool is_displayable(char c){
+    return (c >= 32 && c <= 126);
+}
 
+void ft_cout(bool int_overf, bool float_overf, bool double_overf, bool impossible, char c, int i, float f, double d){
+    if (impossible){
+        std::cout << "char: impossible\n" << "int: impossible\n" << "float: nanf\n" << "double: nan" << std::endl;
+        return ;
+    }
+    if (!is_displayable(c))
+        std::cout << "char: Non displayable" << std::endl;
+    else
+        std::cout << "char: " << "\'" << c << "\'" << std::endl;
+    if (int_overf)
+        std::cout << "int: impossible" << std::endl;
+    else
+        std::cout << "int: " << i << std::endl;
+    if (float_overf)
+        std::cout << "float: impossible" << std::endl;
+    else
+        std::cout << "float: " << std::fixed << std::setprecision(2) << f << "f" << std::endl;
+    if (double_overf)
+        std::cout << "double: impossible" << std::endl;
+    else
+        std:: cout << "double: " << std::setprecision(2) << d << std::endl;
+        
 }
 
 void ScalarConverter::convert(std::string literal){
-    std::string info;
     char c = 0;
     int i = 0;
     float f = 0.0f;
     double d = 0.0;
     bool impossible = false;
+    bool int_overf = false, float_overf = false, double_overf = false;
+    std::stringstream store(literal);
     if (literal == "-inff" || literal == "+inff" || literal == "nanf" || \
         literal == "-inf" || literal == "+inf" || literal == "nan")
-            impossible = true;
+        impossible = true;
     if (isChar(literal)){
-        std::cout << "char" << std::endl;
-        info = "char";
-        c = literal[0];§
+        c = literal[0];
         i = static_cast<int>(c);
         f = static_cast<float>(c);
         d = static_cast<double>(c);
     }
-    else if (isInt(literal))
-        std::cout << "int" << std::endl;
-    else if (isFloat(literal))
-        std::cout << "float" << std::endl;
-    else if(isDouble(literal))
-        std::cout << "double" << std::endl;
+    else if (isInt(literal)){
+        store >> i;
+        if (store.fail())
+            int_overf = true;
+        c = static_cast<char>(i);
+        f = static_cast<float>(i);
+        d = static_cast<double>(i);
+    }
+    else if (isFloat(literal)){
+        store >> f;
+        if (store.fail())
+            float_overf = true;
+        c = static_cast<char>(f);
+        int k;
+        store >> k;
+        if (store.fail())
+            int_overf = true;
+        else
+            i = static_cast<int>(f);
+        d = static_cast<double>(f);
+    }
+    else if(isDouble(literal)){
+        std::cout << "double\n";
+        store >> d;
+        if (store.fail())
+            double_overf = true;
+        c = static_cast<char>(d);
+        int j;
+        store >> j;
+        if (store.fail())
+            int_overf = true;
+        else
+            i = static_cast<int>(d);  
+        float p;
+        store >> p;
+        if (store.fail())
+            float_overf = true;
+        else
+            f = static_cast<float>(d);
+    }
     else
-        std::cout << "tkhwira" << std::endl;
-    ft_cout(info, impossible, c,i,f,d);
+        impossible = true;
+    ft_cout(int_overf, float_overf, double_overf, impossible, c, i, f, d);
 }
